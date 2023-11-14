f02b4f25002e1c07e61b2edcc39d1631f8f12868
beerbumm <chelseaw.gfx@gmail.com>
2023-11-14 15:27:31 -0800

    rendered word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
+import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyWord] = useState("");
+  let [results, setResults] = useState(null);
 
-  function hadleResponse(response) {}
+  function hadleResponse(response) {
+    setResults(response.data[0]);
+  }
 
   function search(event) {
     event.preventDefault();
@@ -20,6 +25,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
